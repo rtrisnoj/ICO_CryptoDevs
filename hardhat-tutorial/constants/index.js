91277b95b7c73153142478a418989404fff04f8b
Ryan Trisnojoyo <RyanTrisnojoyo@Ryans-MacBook-Pro-3.local>
2022-10-31 15:12:11 +0700

    first commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x20ac24e48AF79F8Ef874A1865e2b4b000d3a5fB3";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
